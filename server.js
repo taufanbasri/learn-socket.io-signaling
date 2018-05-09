d281d349a17a2df527b8650a6b90e6882dabc001
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-05-09 11:00:39 +0700

    Setting up socket.io part 2: sending chat with socket.io

--- server.js
+++ server.js
@@ -19,4 +19,11 @@ app.io.route('ready', function (req) {
     })
 })
 
+app.io.route('send', function (req) {
+    app.io.room(req.data.room).broadcast('message', {
+        message: req.data.message,
+        author: req.data.author
+    })
+})
+
 app.listen(PORT);
